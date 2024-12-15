7dec88a16cf723e706b81cf81fc940d1b68f1ae3
Yunkai Zhang <zhykoties@gmail.com>
2024-12-15 15:22:40 -0800

    Delete command.sh

--- command.sh
+++
@@ -1,3 +0,0 @@
-source /miniconda/etc/profile.d/conda.sh
-conda activate iris
-nohup jupyter lab --ip=0.0.0.0 --port=$NOTEBOOK_PORT  --no-browser --allow-root --NotebookApp.token="" --NotebookApp.password="" --FileContentsManager.checkpoints_kwargs="root_dir=${BOLT_ARTIFACT_DIR}" && sleep infinity
